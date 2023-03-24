7f1d434c375475bc11b9b3c034a6e2476b83bd5f
Tretyakov Ivan <timmitarh@mail.ru>
2023-03-24 16:06:04 +0300

    Completed task # 68

---
+++ Task_68/Program.cs
@@ -0,0 +1,41 @@
+﻿/* Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29 */
+/////////////////////////////////////////////////////////////////////////////////////////////////
+Console.Clear();
+ulong m = GetNumberFromUser("Введите неотрицательное целое значение m: ", "Ошибка ввода!");
+ulong n = GetNumberFromUser("Введите неотрицательное целое значение n: ", "Ошибка ввода!");
+Console.WriteLine($"m = {m}, n = {n} -> A(m, n) = {GetAkkermanFanction(m, n)}");
+//////////////////////////////////////////////////////////////////////////////////
+// Описание методов
+// Запрашивает у пользователя целое неотрицательное число
+ulong GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = ulong.TryParse(Console.ReadLine(), out ulong userNumber);
+        if (isCorrect && userNumber >= 0)
+        {
+            return userNumber;
+        }
+        Console.WriteLine(errorMessage);
+    }
+}
+// рекурсионный метод для вычисления функции Аккермана
+ulong GetAkkermanFanction(ulong m, ulong n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0)
+    {
+        return GetAkkermanFanction(m - 1, 1);
+    }
+    else
+    {
+        return GetAkkermanFanction(m - 1, GetAkkermanFanction(m, n - 1));
+    }
+}
